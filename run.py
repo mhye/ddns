1da9e466a2e97fe4fa9855ed2662c1e4625c9dd5
mhye <mhye@home.com>
2017-01-01 01:10:21 +0800

    添加日志

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
-#参数
+from library.domain import Dnsapi
+from library import catchip
+from time import sleep
+import logging,sys
+import logging.handlers
 
 params = {
     "login_token":"你的API Token",
@@ -8,11 +12,30 @@ params = {
     "record_line":"默认",#这里可以不用修改
     }
 
-from library.domain import Dnsapi
-from library import catchip
-from time import sleep
+#from params import params
+#日志初始化
+logger = logging.getLogger(__name__)
+formatter = logging.Formatter('%(asctime)s %(levelname)-8s: %(message)s')
+if sys.platform.startswith("windows"):
+    filename = "./dnspod.log"
+else:
+    filename = "/var/log/dnspod.log"
+file_handler = logging.handlers.RotatingFileHandler(
+                            filename=filename,
+                            mode='a',
+                            maxBytes=1024*1024
+                            )
+file_handler.setFormatter(formatter)
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setFormatter(formatter)
 
-def get_localip(i):
+#安装log
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
+logger.setLevel(logging.INFO)
+
+
+def get_localip(i,logger=None):
     fun_dic = dict(
         taobao = catchip.getip_taobao,
         ip138 = catchip.getip_138,
@@ -20,10 +43,12 @@ def get_localip(i):
         )
     fun_list = ["dnspod","taobao","ip138"]
     while i <= 2:
+        logger.debug("开始使用{} 获取ip".format(fun_list[i]))
         try:
             ip = fun_dic.get(fun_list[i])()
         except Exception as e:
-            print(e)
+            if logger:
+                logger.exception("{} 获取ip失败，详情如下:".format(fun_list[i]) )
             i += 1
             continue
         else:
@@ -33,29 +58,34 @@ def get_localip(i):
 
 if __name__ == "__main__":
 
-    i = 0
-    min = 0
-    sleep_time = 20
+    i = 0#本地ip查询
+    min = 0#计时器60min一次循环
+    sleep_time = 20#循环持续时间
     record = Dnsapi(**params)
-    server_ip = record.get_ip()
     while True:
-        local_ip,i = get_localip(i)
+        local_ip,i = get_localip(i,logger)
         if local_ip is None:
             sleep(sleep_time) #休息60秒，进入下一轮循环
+            logger.warning("本轮获取本地ip失败，进入下一轮")
             continue
-        if min = 59:
+        if min == 0:
+            logger.info("每60分钟开始获取一次服务器ip")
             try:
                 server_ip = record.get_ip()
             except Exception as e:
-                print(e)
+                logger.exception("获取服务器ip失败")
                 sleep(sleep_time)
                 continue
-        print("本地ip：%s，服务器ip：%s" %(local_ip,server_ip))
         if local_ip == server_ip:
-            print("本地ip与服务器一致，无需更新")
+            if min < 6:
+                logger.info("本地ip:{}与服务器ip{}一致，无需更新".format(local_ip,server_ip))
+            if min == 6:
+                logger.info("过滤重复5次以上日志信息...")
+
         else:
-            print("本地服务器与服务器ip不一致")
+            logger.info("本地ip{}与服务器ip{}不一致,开始更新".format(local_ip,server_ip))
             record.update_ip(local_ip)
-            serer_ip = local_ip
-        i = (i+1) % 60
+            server_ip = local_ip
+            min = 0
+        min = (min+1) % 60
         sleep(sleep_time)

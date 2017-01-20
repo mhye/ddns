7abd73b1aede55159d58fea4a4f7802d180cf4e2
mhye_home <mhye@qq.com>
2017-01-20 15:40:42 +0800

    将获取参数的方式简化为只填写tokenid和域名，子域名

--- run.py
+++ run.py
@@ -1,18 +1,17 @@
 from library.domain import Dnsapi
 from library import catchip
+import dump_params
 from time import sleep
 import logging,sys
 import logging.handlers
 
-params = {
+para = {
     "login_token":"你的API Token",
-    "domain_id":"域名id",
-    "record_id":"记录id(主机id)",
+    "domain_name":"域名",#你的域名 例如：abc.com
     "sub_domain":"主机名",#你的子域名 比如www.abc.com,则填"www"
-    "record_line":"默认",#这里可以不用修改
     }
-
-#from params import params
+#from params import  para
+data = dump_params.get_para(**para)
 
 #logger初始化
 logger = logging.getLogger(__name__)
@@ -37,35 +36,34 @@ logger.addHandler(console_handler)
 logger.setLevel(logging.INFO)
 
 
-def get_localip(i,logger=None):
+def get_localip(logger=None):
     fun_dic = dict(
         taobao = catchip.getip_taobao,
         ip138 = catchip.getip_138,
         dnspod = catchip.getip_dnspod,
         )
     fun_list = ["dnspod","taobao","ip138"]
-    while i <= 2:
-        logger.debug("开始使用{} 获取ip".format(fun_list[i]))
+    for item in fun_list:
+        logger.debug("开始使用{} 获取ip".format(item))
         try:
-            ip = fun_dic.get(fun_list[i])()
+            ip = fun_dic.get(item)()
         except Exception as e:
             if logger:
-                logger.exception("{} 获取ip失败，详情如下:".format(fun_list[i]) )
-            i += 1
+                logger.exception("通过{} 获取ip失败，详情如下:".format(item) )
             continue
         else:
-            return (ip,i)
-    return (None,i%3)
+            return (ip)
+    return None
 
 
 if __name__ == "__main__":
 
-    i = 0#本地ip查询
     min = 0#计时器60min一次循环
     sleep_time = 60#循环持续时间
-    record = Dnsapi(**params)
+    record = Dnsapi(**data)
+
     while True:
-        local_ip,i = get_localip(i,logger)
+        local_ip = get_localip(logger)
         if local_ip is None:
             sleep(sleep_time) #休息60秒，进入下一轮循环
             logger.warning("本轮获取本地ip失败，进入下一轮")
